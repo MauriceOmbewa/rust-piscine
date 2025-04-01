e217166737cf836796fe4120a48dfa45e5297d13
mombewa <ombewamaurice79@gmail.com>
2025-04-01 15:27:44 +0300

    add scalar

---
+++ scalar/src/lib.rs
@@ -0,0 +1,30 @@
+pub fn sum(left: u8, right: u8) -> u8 {
+    left + right
+}
+
+pub fn diff(left: i8, right: i8) -> i8 {
+    left - right
+}
+
+pub fn pro(left: u32, right: u32) -> u32 {
+    left * right
+}
+
+pub fn quo(left: f32, right: f32) -> f32 {
+    left / right
+}
+
+pub fn rem(left: f32, right: f32) -> f32 {
+    left % right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
