2bf0170bab2ca35871a924b08d059f1be701df71
mombewa <ombewamaurice79@gmail.com>
2025-04-02 11:43:46 +0300

    update looping

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -7,6 +7,8 @@ fn main(){
     loop{
         let mut input = String::new();
         count += 1;
+
+        println!("I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?");
     
         io::stdin()
             .read_line(&mut input)
