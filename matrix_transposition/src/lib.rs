9c86a35df89ccb52f27da5536ccbe4e1d1ab0c46
mombewa <ombewamaurice79@gmail.com>
2025-04-02 15:50:59 +0300

    update matrix_transportation

--- matrix_transposition/src/lib.rs
+++ matrix_transposition/src/lib.rs
@@ -1,7 +1,7 @@
-#[derive(Debug, PartialEq, Eq)] // Derive Debug, PartialEq, and Eq
-pub struct Matrix(pub(i32, i32), pub(i32, i32));
+#[derive(Debug, PartialEq, Eq)] // Deriving Debug, PartialEq, and Eq traits
+pub struct Matrix(pub (i32, i32), pub (i32, i32));
 
-// Implement the transpose function
-pub fn transpose(m: &Matrix) -> Matrix {
+// Transpose function takes ownership of `Matrix`
+pub fn transpose(m: Matrix) -> Matrix {
     Matrix((m.0 .0, m.1 .0), (m.0 .1, m.1 .1))
 }
