903437e7b8bd187d8254048cae06e48c61758fad
mombewa <ombewamaurice79@gmail.com>
2025-04-03 14:59:38 +0300

    update name_initials

--- name_initials/src/lib.rs
+++ name_initials/src/lib.rs
@@ -3,13 +3,23 @@ pub fn initials(names: Vec<&str>) -> Vec<String> {
 
     for name in names {
         let mut initials = String::new();
+        let mut count = 1;
         
         // Split the name by spaces
         for part in name.split_whitespace() {
             if let Some(first_char) = part.chars().next() {
+                if count == 2 {
+                    count = 1;
+                    // Add the first character followed by a full stop
+                    initials.push(first_char);
+                    initials.push('.');
+                    continue;
+                }
                 // Add the first character followed by a full stop
                 initials.push(first_char);
                 initials.push('.');
+                initials.push(' ');
+                count += 1;
             }
         }
         
