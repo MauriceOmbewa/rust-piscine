167593546f78d6b309f7c0fde3aa2a6d04b33372
mombewa <ombewamaurice79@gmail.com>
2025-04-05 15:56:24 +0300

    add alias

---
+++ changes/src/lib.rs
@@ -0,0 +1,22 @@
+pub struct Light {
+	pub alias: String,
+	pub brightness: u8,
+}
+
+impl Light {
+	pub fn new(alias: &str) -> Self {
+        let temp = Light{
+            alias: String::from(alias),
+            brightness: 0,
+        };
+        temp
+	}
+}
+
+pub fn change_brightness(lights: &mut [Light], alias: &str, value: u8) {
+    for l in lights{
+        if l.alias == alias{
+            l.brightness = value;
+        }
+    }
+}
