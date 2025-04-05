ab6ff843840acccd43f92099f0a097b8b7552459
Maurice <ombewamaurice79@gmail.com>
2025-04-05 23:32:19 +0300

    add arrays

---
+++ arrays/src/lib.rs
@@ -0,0 +1,14 @@
+pub fn sum(a: &[i32]) -> i32 {
+   let mut result = 0;
+
+    for num in a.iter() {
+        result += num;
+    }
+
+    result
+}
+
+pub fn thirtytwo_tens() -> [i32; 32] {
+    let array: [i32; 32] = [10; 32];
+    array
+}
