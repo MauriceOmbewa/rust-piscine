918d8776807f804b897de1578e1234cf025ffea1
mombewa <ombewamaurice79@gmail.com>
2025-04-08 15:50:28 +0300

    corrected panic call for case error in NotFound

--- unwrap_or_expect/src/lib.rs
+++ unwrap_or_expect/src/lib.rs
@@ -8,23 +8,16 @@ pub enum Security {
 
 pub fn fetch_data(server: Result<&str, &str>, security_level: Security) -> String {
     match security_level {
-        Security::Unknown => match server {
-            Ok(value) => value.to_string(),
-            Err(_) => panic!(),
-        }
-        Security::Message => match server {
-            Ok(value) => value.to_string(),
-            Err(_) => panic!("ERROR: program stops"),
-        }
+        Security::Unknown => server.unwrap().to_string(), // Panics with default message if Err
+        Security::Message => server.expect("ERROR: program stops").to_string(), // Panics with custom message
         Security::Warning => server.unwrap_or("WARNING: check the server").to_string(),
         Security::NotFound => match server {
             Ok(value) => value.to_string(),
             Err(err) => panic!("Not found: {}", err),
-        }
+        },
         Security::UnexpectedUrl => match server {
-            Ok(err) => panic!("{}", err.to_string()),
+            Ok(value) => panic!("{}", value),
             Err(err) => err.to_string(),
-        }
-        
+        },
     }
 }
