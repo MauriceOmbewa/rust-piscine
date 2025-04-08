803c2c31c5ee9eca05b28d46917034e9e3f2ec54
mombewa <ombewamaurice79@gmail.com>
2025-04-08 16:09:26 +0300

    add panic

---
+++ panic/src/lib.rs
@@ -0,0 +1,10 @@
+use std::fs::File;
+
+pub fn open_file(s: &str) -> File {
+    let example_file = File::open(s);
+
+    match example_file{
+        Ok(file) => file,
+        Err(_) => panic!(),
+    }
+}
