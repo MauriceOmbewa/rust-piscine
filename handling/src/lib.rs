ac11960133c2766ede8b432ada5fb5ed67a93942
Maurice <ombewamaurice79@gmail.com>
2025-04-08 21:41:41 +0300

    update handling

--- handling/src/lib.rs
+++ handling/src/lib.rs
@@ -1,19 +1,30 @@
 use std::path::Path;
-use std::io::Write;
 use std::fs::OpenOptions;
+use std::io::Write;
 
 pub fn open_or_create<P: AsRef<Path>>(path: &P, content: &str) {
-    let file = OpenOptions::new().append(true).create(true).open(path);
+    let mut file = OpenOptions::new()
+        .append(true)   // Append to the file.
+        .create(true)   // Create the file if it doesn't exist.
+        .open(path)
+        .expect("Failed to open or create the file");
 
-    match file{
-        Ok(mut okay_result) => {
-            let words = writeln!(okay_result, "{}", content);
-
-            match words{
-                Ok(_) => {},
-                Err(_) => panic!(),
-            }
-        }
-        Err(_) => panic!(),
-    }
+    writeln!(file, "{}", content)
+        .expect("Failed to write to the file");
 }
+
+// pub fn open_or_create<P: AsRef<Path>>(path: &P, content: &str) {
+//     let file = OpenOptions::new().append(true).create(true).open(path);
+
+//     match file{
+//         Ok(mut okay_result) => {
+//             let words = writeln!(okay_result, "{}", content);
+
+//             match words{
+//                 Ok(_) => {},
+//                 Err(_) => panic!(),
+//             }
+//         }
+//         Err(_) => panic!(),
+//     }
+// }
